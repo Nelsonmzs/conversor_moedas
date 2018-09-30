b52db69a8225f51e47950ca0b1fef49acdc3217f
Nelsonmzs <nelson30_8@hotmail.com>
2018-09-30 20:58:24 -0300

    - Configura requisição API https://api.hgbrasil.com/finance/ - Adiciona mensagem de 'aguarde', enquanto a requisição esta sendo efetuada - Adiciona mensagens amigaveis de sucesso e/ou falha na requisição

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
-void main() {
+const request =
+    "https://api.hgbrasil.com/finance/quotations?format=json&key=7b656341 ";
+
+void main() async {
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  double dolar;
+  double euro;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black26,
+      appBar: AppBar(
+        title: Text("Conversor"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    "Carregando, aguarde...",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                      child: Text(
+                    "Ocorreu um erro ao carregar os dados. =(",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ));
+                } else {
+                   dolar  = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                   euro   = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                }
+            }
+          }),
+    );
+  }
 }
